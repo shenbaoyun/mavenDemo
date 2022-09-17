574639c565a460f93b94b85e9ccc742513430692
shenbaoyun <shenbaoyun99@gmail.com>
2022-09-17 11:27:42 +0800

    首次测试代码提交

--- pro01/src/main/java/com/shenbaoyun/git/GitTest.java
+++ pro01/src/main/java/com/shenbaoyun/git/GitTest.java
@@ -7,5 +7,7 @@ package com.shenbaoyun.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello github");
+        System.out.println("hello gitee");
     }
 }
