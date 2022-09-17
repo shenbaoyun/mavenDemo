14c40e826da954604b999795e157a6de9a47c263
shenbaoyun <shenbaoyun99@gmail.com>
2022-09-17 11:49:18 +0800

    hot-fix 代码提交1

--- pro01/src/main/java/com/shenbaoyun/git/GitTest.java
+++ pro01/src/main/java/com/shenbaoyun/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello gitee");
         System.out.println("hello gitee");
         System.out.println("hello shenbao");
+        System.out.println("hot-fix test");
     }
 }
